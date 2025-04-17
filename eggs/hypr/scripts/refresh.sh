24bc18d1a9127f2e68d955a81e3d144a159dee8e
bannaa <holy-bannaa@proton.me>
2025-04-17 18:39:09 +0300

    [hypr] script to refresh apps after config changes

---
+++ eggs/hypr/scripts/refresh.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Scripts for refreshing ags waybar, rofi, swaync, wallust
+
+SCRIPTSDIR=$HOME/.config/hypr/scripts
+
+# Define file_exists function
+file_exists() {
+    if [ -e "$1" ]; then
+        return 0  # File exists
+    else
+        return 1  # File does not exist
+    fi
+}
+
+#Restart waybar
+pkill waybar && waybar
+
+# relaunch swaync
+sleep 0.5
+swaync > /dev/null 2>&1 &
+
+exit 0
