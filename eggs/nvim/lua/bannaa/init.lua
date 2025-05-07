5ae9b587dbd273725025b9a9466568a00e8d5510
bannaa <holy-bannaa@proton.me>
2025-05-07 23:43:36 +0300

    [nvim] fix indentation

--- eggs/nvim/lua/bannaa/init.lua
+++ eggs/nvim/lua/bannaa/init.lua
@@ -13,7 +13,7 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
 -- something to do with files
-vim.cmd('filetype plugin indent on')
+vim.cmd("filetype plugin indent on")
 
 -- the very cool nerd font
 vim.g.nerd_font = true
@@ -26,7 +26,7 @@ vim.opt.relativenumber = true
 
 -- yank to system clipboard
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
+    vim.opt.clipboard = "unnamedplus"
 end)
 
 -- break indent
@@ -57,16 +57,16 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- highlight when yanking (copying) text
 -- see `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- treat .rhai files as rust files
 vim.filetype.add({
-	extension = {
-		rhai = "rust",
-	},
+    extension = {
+        rhai = "rust",
+    },
 })
