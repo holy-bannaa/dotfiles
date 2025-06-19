dcb74f5c49e97c0b48198a302e2a14692525216e
bannaa <holy-bannaa@proton.me>
2025-06-19 12:33:55 +0300

    [nvim] reformat and add love2d lsp

--- eggs/nvim/lua/plugins/lsp.lua
+++ eggs/nvim/lua/plugins/lsp.lua
@@ -1,38 +1,49 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "clangd",
-                "pyright",
-                "ts_ls",
-                "rust_analyzer",
-                "gopls",
-            },
-        })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"clangd",
+				"pyright",
+				"ts_ls",
+				"rust_analyzer",
+				"gopls",
+			},
+		})
 
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        local lspconfig = require("lspconfig")
+		local lspconfig = require("lspconfig")
 
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.rust_analyzer.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.clangd.setup({
-            capabilities = capabilities,
-        })
-        lspconfig.hyprls.setup({
-            capabilities = capabilities,
-        })
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					workspace = { checkThirdParty = false },
+					telemetry = { enable = false },
+					library = {
+						"${3rd}/love2d/library",
+					},
+				},
+			},
+		})
 
-    end,
+		lspconfig.rust_analyzer.setup({
+			capabilities = capabilities,
+		})
+
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+		})
+
+		lspconfig.hyprls.setup({
+			capabilities = capabilities,
+		})
+	end,
 }
